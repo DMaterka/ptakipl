b8d6d3faafdd91dff699bbe00e5b1654e4a15c39
Roberto <rtuziak@gmail.com>
2024-06-02 19:22:18 +0200

    changes 02.06.2024B

---
+++ src/services/api.conf.js
@@ -0,0 +1,4 @@
+
+const ApiConfig = { baseURL :  "https://api.ebird.org/v2/" }
+
+export default ApiConfig;
---
+++ src/services/birdspecies.service.js
@@ -0,0 +1,13 @@
+import ApiService from "./api.service.js";
+
+const birdspecies = {
+    load: async function (birdspecies){
+        let response = await ApiService.get("product/spplist/PL/",birdspecies);
+        console.log("dupa");
+        console.log(response);
+        return response.data;
+
+    }
+
+}
+export default birdspecies;
