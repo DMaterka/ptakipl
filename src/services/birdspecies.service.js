4347bd4771a028d4d71f1a098d19e90b9080b86d
Roberto <rtuziak@gmail.com>
2024-06-09 20:07:25 +0200

    09.06.2024 Working API

--- src/services/birdspecies.service.js
+++ src/services/birdspecies.service.js
@@ -1,11 +1,18 @@
 import ApiService from "./api.service.js";
 
 const birdspecies = {
-    load: async function (birdspecies){
-        let response = await ApiService.init().get("product/spplist/PL/",birdspecies);
-        console.log("dupa");
-        console.log(response);
+    load: async function (speciesArray) {
+        const species = speciesArray.join(',');
+        const params = {
+            species: species,
+            locale: 'pl',
+            fmt: 'json'
+        };
+
+        let response = await ApiService.init().get("ref/taxonomy/ebird", params);
+
         return response.data;
     }
-}
+};
+
 export default birdspecies;
