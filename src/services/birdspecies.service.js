47a876890ef4ac95dfcbbd9a73be8facc75d2c58
Roberto <rtuziak@gmail.com>
2024-06-16 19:59:25 +0200

    16.06.2024 Bird list API correction

--- src/services/birdspecies.service.js
+++ src/services/birdspecies.service.js
@@ -4,6 +4,7 @@ const birdspecies = {
     load: async function (birdspecies){
         let response = await ApiService.init().get("product/spplist/PL/",birdspecies);
         console.log("dupa");
+        console.log("dupa");
         console.log(response);
         return response.data;
     }
